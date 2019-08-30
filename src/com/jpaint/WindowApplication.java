ed6f6f66dccf17114fd3c3eee93372f5ad061f92
Andrew Yaros <aey24@drexel.edu>
2019-08-30 13:55:34 -0400

    Moved menu setup and functions into menumanager

--- src/com/jpaint/WindowApplication.java
+++ src/com/jpaint/WindowApplication.java
@@ -1,20 +1,10 @@
 package com.jpaint;
 
-import javax.imageio.ImageIO;
-import javax.print.attribute.HashPrintRequestAttributeSet;
-import javax.print.attribute.PrintRequestAttributeSet;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.awt.print.Printable;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
-import java.io.IOException;
-import java.util.Hashtable;
 
 class WindowApplication {
 //parameters for certain UI sizes
@@ -38,6 +28,7 @@ private JLabel sizeLabel;
 private ImageModel theModel;
 private ImageView theView;
 private ManageFiles manageFiles;
+private ManageMenus manageMenus;
 
 WindowApplication() {
 }
@@ -198,147 +189,7 @@ void WindowSetup(int width, int height) {
 	manageFiles = new ManageFiles(theModel, mainFrame);
 	
 	/*====== MENU BAR ======*/
-	
-	JMenuBar menuBar = new JMenuBar();
-	
-	JMenu fileMenu = new JMenu("File");
-	menuBar.add(fileMenu);
-	
-	JMenu editMenu = new JMenu("Edit");
-	menuBar.add(editMenu);
-	
-	JMenu imageMenu = new JMenu("Image");
-	menuBar.add(imageMenu);
-	
-	JMenu helpMenu = new JMenu("Help");
-	menuBar.add(helpMenu);
-	
-	//add the menubar
-	mainFrame.setJMenuBar(menuBar);
-	
-	/*====== MENU ITEMS ======*/
-	
-	Hashtable<String, MenuItem> menuItems = new Hashtable<>();
-	
-	int cmdCtrlModifier = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
-	int cmdCtrlShiftModifier = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask() | InputEvent.SHIFT_DOWN_MASK;
-	
-	//file menu
-	menuItems.put("new", new MenuItem("New Opaque Image", KeyEvent.VK_N, cmdCtrlModifier, fileMenu, KeyEvent.VK_N,
-	                                  e -> manageFiles.newFile(false)
-	));
-	
-	menuItems.put(
-		  "newtrans",
-		  new MenuItem("New Transparent Image", KeyEvent.VK_N, cmdCtrlShiftModifier, fileMenu, KeyEvent.VK_T,
-		               e -> manageFiles.newFile(true)
-		  )
-	             );
-	fileMenu.addSeparator();
-	
-	menuItems.put("open", new MenuItem("Open", KeyEvent.VK_O, cmdCtrlModifier, fileMenu, KeyEvent.VK_O,
-	                                   e -> manageFiles.openFile()
-	));
-	menuItems.put("save", new MenuItem("Save", KeyEvent.VK_S, cmdCtrlModifier, fileMenu, KeyEvent.VK_S,
-	                                   e -> manageFiles.save()
-	));
-	menuItems.put("saveas", new MenuItem("Save As", KeyEvent.VK_S, cmdCtrlShiftModifier, fileMenu, KeyEvent.VK_A,
-	                                     e -> manageFiles.saveas()
-	));
-	fileMenu.addSeparator();
-	
-	menuItems
-		  .put("print", new MenuItem("Print", KeyEvent.VK_P, cmdCtrlModifier, fileMenu, KeyEvent.VK_P,
-		                             e -> print()
-		  ));
-	fileMenu.addSeparator();
-	
-	if (Main.IS_MAC)
-		menuItems.put("close", new MenuItem("Close", KeyEvent.VK_W, cmdCtrlModifier, fileMenu, KeyEvent.VK_W,
-		                                    e -> manageFiles.exit()
-		));
-	else
-		menuItems.put("exit", new MenuItem("Exit", KeyEvent.VK_E, cmdCtrlModifier, fileMenu, KeyEvent.VK_E,
-		                                   e -> manageFiles.exit()
-		));
-	
-	//edit menu
-	menuItems.put("undo", new MenuItem("Undo", KeyEvent.VK_Z, cmdCtrlModifier, editMenu, KeyEvent.VK_Z,
-	                                   e -> undo()
-	));
-	
-	MenuItem redoItem; //shortcut changes depending on platform
-	if (Main.IS_MAC)
-		redoItem = new MenuItem("Redo", KeyEvent.VK_Z, cmdCtrlShiftModifier, editMenu, KeyEvent.VK_Z, e -> redo());
-	else redoItem = new MenuItem("Redo", KeyEvent.VK_Y, cmdCtrlModifier, editMenu, KeyEvent.VK_Y, e -> redo());
-	menuItems.put("redo", redoItem);
-	
-	editMenu.addSeparator();
-	
-	menuItems.put("selectall", new MenuItem("Select All", KeyEvent.VK_A, cmdCtrlModifier, editMenu, KeyEvent.VK_A,
-	                                        e -> dummy()
-	));
-	editMenu.addSeparator();
-	
-	menuItems.put("cut", new MenuItem("Cut", KeyEvent.VK_X, cmdCtrlModifier, editMenu, KeyEvent.VK_X,
-	                                  e -> dummy()
-	));
-	menuItems.put("copy", new MenuItem("Copy", KeyEvent.VK_C, cmdCtrlModifier, editMenu, KeyEvent.VK_C,
-	                                   e -> dummy()
-	));
-	menuItems.put("paste", new MenuItem("Paste", KeyEvent.VK_V, cmdCtrlModifier, editMenu, KeyEvent.VK_V,
-	                                    e -> dummy()
-	));
-	
-	//image menu
-	menuItems.put("resize", new MenuItem("Resize", imageMenu, KeyEvent.VK_R, e -> resize()));
-	imageMenu.addSeparator();
-	
-	menuItems.put("fliph", new MenuItem("Flip Image Horizontally", imageMenu, KeyEvent.VK_H,
-	                                    e -> flip(true)
-	));
-	menuItems.put("flipv", new MenuItem("Flip Image Vertically", imageMenu, KeyEvent.VK_V,
-	                                    e -> flip(false)
-	));
-	imageMenu.addSeparator();
-	
-	menuItems.put("rotateleft", new MenuItem("Rotate Left 90\u00B0", imageMenu, KeyEvent.VK_L,
-	                                         e -> rotate(Canvas.Transform.ROTATE_LEFT)
-	));
-	menuItems.put("rotateright", new MenuItem("Rotate Right 90\u00B0", imageMenu, KeyEvent.VK_R,
-	                                          e -> rotate(Canvas.Transform.ROTATE_RIGHT)
-	));
-	menuItems.put("rotate180", new MenuItem("Rotate 180\u00B0", imageMenu, KeyEvent.VK_U,
-	                                        e -> rotate(Canvas.Transform.ROTATE_180)
-	));
-	
-	menuItems.put("about", new MenuItem("About " + APPLICATION_NAME, helpMenu, KeyEvent.VK_U, e -> {
-		Image image = null;
-		try {
-			image = ImageIO.read(getClass().getResource("icons/appicon.png"));
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-		try {
-			if (image != null) {
-				image = image.getScaledInstance(116, 116, java.awt.Image.SCALE_SMOOTH);
-			}
-		} catch (NullPointerException ignored) { }
-		ImageIcon appIcon;
-		if (image == null) appIcon = null;
-		else appIcon = new ImageIcon(image);
-		
-		JOptionPane.showMessageDialog(mainFrame, APPLICATION_NAME +
-		                                         " is a fun painting program \n" +
-		                                         "inspired by Apple's MacPaint (from \n" +
-		                                         "1984) and old versions of Microsoft \n" +
-		                                         "Paint. Created by Andrew Yaros in \n" +
-		                                         "2019, originally for the CS 338 \n" +
-		                                         "(GUIs) class at Drexel University.",
-		                              "About " + APPLICATION_NAME, JOptionPane.INFORMATION_MESSAGE,
-		                              appIcon
-		                             );
-	}));
+	manageMenus = new ManageMenus(theModel, mainFrame, manageFiles);
 	
 	/*====== SET TITLE ======*/
 	manageFiles.setTitle(NEW_DOCUMENT);
@@ -364,37 +215,4 @@ void WindowSetup(int width, int height) {
 	System.out.println("Loaded.");
 }
 
-//undo/redo
-private void undo() { theModel.undo(); }
-
-private void redo() { theModel.redo(); }
-
-//cross platform print dialog based on java example
-private void print() {
-	PrinterJob printJob = PrinterJob.getPrinterJob();
-	printJob.setPrintable((graphics, pageFormat, pageIndex) -> {
-		if (pageIndex != 0) { return Printable.NO_SUCH_PAGE; }
-		graphics.drawImage(theModel.getImage(), 0, 0, theModel.getWidth(), theModel.getHeight(), null);
-		return Printable.PAGE_EXISTS;
-	});
-	PrintRequestAttributeSet attributeSet = new HashPrintRequestAttributeSet();
-	printJob.pageDialog(attributeSet);
-	boolean isOK = printJob.printDialog(attributeSet);
-	if (isOK) try { printJob.print(attributeSet); } catch (PrinterException e) { e.printStackTrace(); }
-}
-
-//resize (crop) image
-private void resize() { new WindowResize(mainFrame, theModel); }
-
-//flip image
-private void flip(boolean horizontally) {
-	if (horizontally) theModel.flip(0);
-	else theModel.flip(1);
-}
-
-//rotate image
-private void rotate(Canvas.Transform option) { theModel.rotate(option); }
-
-//temp
-private void dummy() { } //temporary for menu listeners
 }
